7788026050cba28ca1482a7b9889919eb08bcb64
Matthew Swain <mswain@gmail.com>
2017-04-16 12:46:06 -0500

    Add Notes, code

---
+++ fibonacci.py
@@ -0,0 +1,20 @@
+import argparse
+from datetime import datetime
+
+def get_args():
+    parser = argparse.ArgumentParser(description='Calculate Nth Fibbonaci Number.')
+    parser.add_argument("n", type=int, help="Nth Fib number")
+    return parser.parse_args()
+
+def F(n):
+    if n == 0: return 0
+    elif n == 1: return 1
+    else: return F(n-1)+F(n-2)
+
+args = get_args()
+start_time = datetime.now()
+solution = F(args.n)
+end_time = datetime.now()
+
+print "{0} fibbonaci number is: {1} found in {2}s ".format(args.n, solution, (end_time-start_time).total_seconds())
+
